6ce50ad3aab5948ec5f8c3a2ef36011fd255b7b4
griff182uk <griff182uk@yahoo.co.uk>
2023-09-17 14:33:01 +0000

    changed lib

--- catalog-generator/catalog_gen.py
+++ catalog-generator/catalog_gen.py
@@ -1,2 +1,6 @@
 import os
-print(os.environ.get('CHATGPT_KEY'))
+import openai
+
+openai.api_key = os.getenv("CHATGPT_KEY")
+
+chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Hello world"}])
