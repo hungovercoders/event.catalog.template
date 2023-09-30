b5e52ea3bb863b8e7140b36892ecb76154112544
griff182uk <griff182uk@yahoo.co.uk>
2023-09-30 14:10:58 +0000

    got basic version working and lots of examples

--- catalog-generator/event_lists/app.py
+++ catalog-generator/event_lists/app.py
@@ -1,51 +1,95 @@
 import os
 import openai
 import requests
+import json
 
 def get_latest_architecture_version(dir_path: str):
-    files = os.listdir('dir_path')
-    return files
+    """Gets latest architecture version from directory
+
+    Args:
+        dir_path (str): The directory path to search
+
+    Returns:
+        str: The architecture version
+    """
+    int_version = 1
+    if(os.path.exists(dir_path)):
+        files = os.listdir(dir_path)
+        for file in files:
+            version = file.split('_')[-1].replace(".json","")
+            int_version = int(version) + 1
+        version_str = str(int_version)
+    version_str = version_str.rjust(3, '0') 
+    return version_str
 
 def get_filepath(architecture_required : str):
-    architecture_required_path = architecture_required.replace(" ","_")
-    dir_path = f"{architecture_required_path}/{architecture_required_path}"
-    files = os.listdir(dir_path)
-    return filepath
+    """Returns filepath for events architecture
 
-def main():
+    Args:
+        architecture_required (str): The system of he software architecture required
+
+    Returns:
+        _type_: The filepath for the event architecture
+    """
+    architecture_path = architecture_required.replace(" ","_")
+    isExist = os.path.exists(architecture_path)
+    if not isExist:
+        os.makedirs(architecture_path)
+        print("The new directory is created!")
+    version = get_latest_architecture_version(architecture_path)
+    file_path = f"{architecture_path}/{architecture_path}_{version}.json"
+    return file_path
+
+def get_event_list(architecture_required : str, template_version: int):
     """
     Main function generates event list
     """
-
-    architecture_required = input()
-
+    
     openai.api_key = os.getenv("CHATGPT_KEY")
+    template_version_str = str(template_version).rjust(3, '0') 
+    
+    with open(f'template/template_{template_version_str}.json') as template_file:
+        json_template = json.load(template_file)
+    
+    answers = []
+    
+    for i in range(1,4):
 
-    json_template = """{\"domain\": \"UserManagement\", \"event\": \"UserRegistered\", \"team\": \"RegistrationTeam\"},
-    {\"domain\": \"DogManagement\", \"event\": \"DogProfileCreated\", \"team\": \"DogManagementTeam\"},
-    {\"domain\": \"DogManagement\", \"event\": \"DogProfileUpdated\", \"team\": \"DogManagementTeam\"},
-    {\"domain\": \"WalkManagement\", \"event\": \"WalkRequested\", \"team\": \"WalkTeam\"},
-    {\"domain\": \"WalkManagement\", \"event\": \"WalkScheduled\", \"team\": \"SchedulingTeam\"},
-    {\"domain\": \"WalkManagement\", \"event\": \"WalkerAssigned\", \"team\": \"SchedulingTeam\"},
-    {\"domain\": \"WalkManagement\", \"event\": \"WalkInitiated\", \"team\": \"WalkerTeam\"},
-    {\"domain\": \"WalkManagement\", \"event\": \"WalkInProgress\", \"team\": \"WalkerTeam\"},
-    {\"domain\": \"WalkManagement\", \"event\": \"WalkCompleted\", \"team\": \"WalkerTeam\"},
-    {\"domain\": \"PaymentManagement\", \"event\": \"PaymentMade\", \"team\": \"FinanceTeam\"},
-    {\"domain\": \"PaymentManagement\", \"event\": \"PaymentReceived\", \"team\": \"FinanceTeam\"},
-    {\"domain\": \"Support\", \"event\": \"SupportTicketRaised\", \"team\": \"CustomerSupportTeam\"},
-    {\"domain\": \"Support\", \"event\": \"SupportTicketClosed\", \"team\": \"CustomerSupportTeam\"}"""
-
+        print(f"Calling API {i} time to get the json architecture for {architecture_required}...")
+        chat_completion = openai.ChatCompletion.create(model="gpt-4", messages=[{"role": "user"
+        , "content": f"""Return the JSON only in your response for an event catalog that supports a software
+        architecture for an {architecture_required} system using this JSON as the example template {json_template} to inform your choices
+        for the software architecture."""}])
+        out = chat_completion['choices'][0]['message']['content']
+        print(f"Called API {i} times to get the json architecture for {architecture_required}.")
+        answers.append(out)
+        
+    print(f"Calling API to ask which is the best json architecture for {architecture_required}...")
     chat_completion = openai.ChatCompletion.create(model="gpt-4", messages=[{"role": "user"
-    , "content": f"""Return the JSON only in your response for an event catalog that supports a software
-    architecture for an {architecture_required} system using this JSON as the example template {json_template}"""}])
+    , "content": f"""Return the JSON only in your response of the best json representation of the software architecture for {architecture_required} that is found in {answers} given the template {json_template}."""}])
+    final_answer = chat_completion['choices'][0]['message']['content']
+    print(f"Called API to ask which is the best json architecture for {architecture_required}...")
+    
+    return final_answer
+    
+def write_event_list(architecture_required  : str, event_list : str):
+    
+    file_path = get_filepath(architecture_required)
+    print(f"The event list json will write out to {file_path}.")
 
-    out = chat_completion['choices'][0]['message']['content']
-
-    architecture_required_path = architecture_required.replace(" ","_")
-
-    with open(f"event_lists/{architecture_required_path}/{architecture_required_path}_v2.json", "w") \
+    print(f"Writing out file to {file_path}...")
+    with open(file_path, "w") \
     as outfile:
-        outfile.write(out)
+        outfile.write(event_list)
+    print(f"Written out file to {file_path}.")
+    
+def main():
+    print('Enter the software architecture you require:')
+    architecture_required = input()
+    print('JSON template version required:')
+    template_version = input()
+    event_list = get_event_list(architecture_required, template_version)
+    write_event_list(architecture_required, event_list)
 
 if __name__ == '__main__':
     main()
